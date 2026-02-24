72aeb9f6ed6a1b12181937fa4085dfe98e8bd851
helenamajer <helena.majer@yahoo.com>
2026-02-24 12:11:55 +0100

    GREEN: InchesToCentimeters throws exception when input is negative.

--- src/ImperialMetricConverter/Converters/LengthConverter.cs
+++ src/ImperialMetricConverter/Converters/LengthConverter.cs
@@ -6,6 +6,9 @@ public class LengthConverter
     // Converting inches to centimeters.
     public double InchesToCentimeters(double inches)
     {
+        if (inches < 0)
+            throw new ArgumentException("Length cannot be negative.");
+        
         return (inches * 2.54);
     }
 }    
