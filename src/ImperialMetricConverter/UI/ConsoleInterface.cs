b61ef2ab48901867d8b6b9ea218ffa7a7207a6e2
helenamajer <helena.majer@yahoo.com>
2026-02-25 23:28:11 +0100

    GREEN: test passes for console interface displaying welcome message.

--- src/ImperialMetricConverter/UI/ConsoleInterface.cs
+++ src/ImperialMetricConverter/UI/ConsoleInterface.cs
@@ -4,14 +4,17 @@ namespace ImperialMetricConverter.UI;
 
 public class ConsoleInterface
 {
+    private readonly ConverterService _service;
+    private readonly IUserIO _inputOutput;
 
     public ConsoleInterface(ConverterService service, IUserIO inputOutput)
     {
-
+        _service = service;
+        _inputOutput = inputOutput;
     }
 
     public void Run()
     {
-
+        _inputOutput.WriteLine("Welcome to the Metric/Imperial Unit Converter!");
     }
 }
