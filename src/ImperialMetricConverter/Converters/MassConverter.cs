5931ab0a1ed20d512efd92dd6f9c5669519c0d70
helenamajer <helena.majer@yahoo.com>
2026-02-25 14:49:17 +0100

    GREEN: test passes for throwing argument exception when passing in a negative value.

--- src/ImperialMetricConverter/Converters/MassConverter.cs
+++ src/ImperialMetricConverter/Converters/MassConverter.cs
@@ -13,6 +13,10 @@ public class MassConverter
 {
     public double Convert(double value, MassUnit from, MassUnit to)
     {
+        // if input is negative, throw argument exception.
+        if (value < 0)
+            throw new ArgumentException("Mass cannot be negative.");
+        
         // formula for pound to kilogram conversion.
         if (from == MassUnit.Pound && to == MassUnit.Kilogram)
             return value * 0.453592;
